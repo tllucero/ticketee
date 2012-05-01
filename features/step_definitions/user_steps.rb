6872ba68db0042b1c83f97f348db0b3039ff3cd7
Thomas Lucero <tllucero@stardevsoft.com>
2012-04-30 20:47:03 -0400

    Lock down specific projects controller actions for admins only

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -18,5 +18,11 @@ Given /^I am signed in as them$/ do
   })
 end
 
+Given /^I am signed in as "([^"]*)"$/ do |email|
+	@user = User.find_by_email!(email)
+	steps("Given I am signed in as them")
+end
+
+
 
 
